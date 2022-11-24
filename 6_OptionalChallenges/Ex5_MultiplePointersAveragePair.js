7d1280ac51a23a41d4be8e0077ab86ef48d19960
Geetham15 <Geetham15@gmail.com>
2022-11-24 10:40:24 -0700

    Average pairs

---
+++ 6_OptionalChallenges/Ex5_MultiplePointersAveragePair.js
@@ -0,0 +1,42 @@
+/*
+Multiple Pointers - averagePair
+Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array 
+where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+
+Bonus Constraints:
+
+Time: O(N)
+
+Space: O(1)
+
+Sample Input:
+
+averagePair([1,2,3],2.5) // true
+averagePair([1,3,3,5,6,7,10,12,19],8) // true
+averagePair([-1,0,3,4,5,6], 4.1) // false
+averagePair([],4) // false
+*/
+function averagePair(arr, num) {
+    let start = 0
+    let end = arr.length - 1
+    let valid = false
+    while(start < end) {
+        let avg = (arr[start] + arr[end])/2
+        if (avg === num) {
+            console.log("Average : " , avg)
+            return true
+        } else if (avg < num) {
+            start++
+        } else {
+            end--
+        }
+    }
+    console.log("Valid : ", valid)
+    valid = false
+    return valid
+}
+
+averagePair([1,2,3],2.5) // true
+averagePair([1,3,3,5,6,7,10,12,19],8) // true
+averagePair([-1,0,3,4,5,6], 4.1) // false
+averagePair([],4) // false
